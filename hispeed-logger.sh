a710c74b4832cfc7e7fdc66df326952450d32ecf
Anton P. Linevich <anton@linevich.com>
2023-12-19 01:21:32 -0800

    use date for default filename

--- hispeed-logger.sh
+++ hispeed-logger.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -xue
+
 port=${1:-/dev/serial0}
 speed=${2:-230400}
 outfile=${3:-serial.log}
+now=$(date "+%Y-%m-%d.%H:%M:%S")
+out=serial-${now}.log
 
 stty -F${port} ${speed}
 (stty raw; cat > ${out} ) < {port}
 
 #stdbuf -o0 cat /dev/serial0 > /var/tmp/serial.log
-
